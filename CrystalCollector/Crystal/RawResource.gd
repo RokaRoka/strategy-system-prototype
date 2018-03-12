e7edca6ed8836d6943623affcf3636656ef02d5b
Samuel Scott <samuel.scott@mymail.champlain.edu>
2018-03-12 12:14:57 -0400

    Crystals now run out of resource! More strategy!

--- CrystalCollector/Crystal/RawResource.gd
+++ CrystalCollector/Crystal/RawResource.gd
@@ -1,20 +1,41 @@
 extends StaticBody2D
 
 #	CONSTANT DATA
-const RESOURCE_THRESHOLD = [300, 100, 0]
+const RESOURCE_THRESHOLD = [200, 90, 0]
 
 #sprite for each threshold
-#const RESOURCE_SPRITES = []
+onready var crystalSprites = [get_node( "Crystal15" ), get_node( "Crystal5" ), get_node( "Crystal7" )]
+onready var clickBox = get_node( "ClickBox" )
 
 #	MEMBER DATA
-var resourceAmount = 500
+var mineable = true
+var resourceAmount = 300
 var resourceThresholdIndex = 0
 
 
+func setInactive():
+	mineable = false
+	clickBox.hide()
+
 #	RESOURCE LOGIC
 
-func mineResource(var miner):
-	#TODO if miner is a Unit
-	#	resourceAmount -= miner.mineAmount
-	#	return miner.mineStrength
-	pass
+func checkResource():
+	if resourceAmount <= RESOURCE_THRESHOLD[resourceThresholdIndex]:
+		#make a sprite disappear
+		crystalSprites[resourceThresholdIndex].hide()
+		resourceThresholdIndex += 1
+
+func mineResource(var amount):
+	var resourceGained
+	resourceAmount -= amount
+
+	checkResource()
+	if resourceAmount <= 0:
+		#fix resource math
+		resourceGained = amount + resourceAmount
+		#set as inactive
+		setInactive()
+	else:
+		resourceGained = amount
+	
+	return resourceGained
